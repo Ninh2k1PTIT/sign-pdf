d58c3835c7b8559b1a85d4ff1f9d68db3661665b
unknown <nguyentienhaininh@gmail.com>
2022-08-25 09:39:24 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,158 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PdfLoadedEvent } from 'ngx-extended-pdf-viewer';
+import { getDocument, GlobalWorkerOptions, version } from 'pdfjs-dist';
+import { forkJoin, from, map, Observable, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'ngxpdf';
+  @ViewChild('item') d1!: ElementRef;
+  src: string = '';
+  files: any = [];
+  totalPages: number = 0;
+  pageNumber: number = 0;
+  choice: number = 0;
+  scale: any = 'auto';
+  scaleOptions = [
+    'auto',
+    'page-actual',
+    'page-fit',
+    'page-width',
+    '50%',
+    '100%',
+    '125%',
+    '150%',
+    '200%',
+    '300%',
+    '400%',
+  ];
+
+  previousItem!: HTMLElement;
+
+  constructor(private modalService: NgbModal) {
+    GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.min.js`;
+  }
+
+  inputFile(ev: any) {
+    this.files = [];
+    let observables = [];
+    for (const file of ev.target.files) {
+      observables.push(this.readFile(file));
+    }
+
+    let source = forkJoin(observables);
+    source.subscribe((data) => {
+      this.files = data;
+      this.src = this.files[0].data;
+    });
+  }
+
+  readFile(
+    file: any
+  ): Promise<{ name: string; data: string | ArrayBuffer | null }> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve({ name: file.name, data: reader.result });
+      reader.onerror = (error) => reject(error);
+    });
+  }
+
+  pdfLoaded(pdf: PdfLoadedEvent) {
+    // this.d1.nativeElement.innerHTML = '';
+    this.totalPages = pdf.pagesCount;
+    this.pageNumber = 1;
+    let observables = [];
+    for (let i = 0; i < this.totalPages; i++)
+      observables.push(this.getPdfThumbnail(this.src, i + 1));
+    let source = forkJoin(observables);
+    source.subscribe((tags) => {
+      for (let [index, value] of tags.entries()) {
+        value.id = index.toString();
+        value.style.marginTop = '10px';
+        value.addEventListener('click', (ev) => {
+          const target = ev.target as HTMLTextAreaElement;
+          this.pageNumber = parseInt(target.id) + 1;
+          this.thumbnailsStyleChange(this.pageNumber);
+        });
+        this.d1.nativeElement.appendChild(value);
+        let foo = document.createElement('div');
+        foo.innerHTML = index + 1 + '';
+        this.d1.nativeElement.appendChild(foo);
+      }
+      this.previousItem = document.getElementById('0')!;
+      this.previousItem.className = 'selected';
+    });
+  }
+
+  getPdfThumbnail(pdfUrl: any, pageNumber: number) {
+    return from(getDocument(pdfUrl).promise).pipe(
+      take(1),
+      switchMap((pdf) => from(pdf.getPage(pageNumber))),
+      switchMap((page) => {
+        const canvas = document.createElement('canvas');
+        const viewport = page.getViewport({ scale: 0.2 });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        return from(
+          page.render({
+            canvasContext: canvas.getContext('2d') as CanvasRenderingContext2D,
+            viewport,
+          }).promise
+        ).pipe(map(() => canvas));
+      }),
+      switchMap((canvas) => {
+        return new Observable<HTMLCanvasElement>((observer) => {
+          observer.next(canvas);
+          observer.complete();
+        });
+      })
+    );
+  }
+
+  fileChange(fileIndex: number) {
+    if (this.choice != fileIndex) {
+      this.choice = fileIndex;
+      this.src = this.files[fileIndex].data;
+    }
+  }
+
+  pageChange(pageIndex: number) {
+    this.thumbnailsStyleChange(pageIndex);
+  }
+
+  nextPage() {
+    if (this.pageNumber < this.totalPages) {
+      this.pageNumber += 1;
+      this.thumbnailsStyleChange(this.pageNumber);
+    }
+  }
+
+  previousPage() {
+    if (this.pageNumber > 1) {
+      this.pageNumber -= 1;
+      this.thumbnailsStyleChange(this.pageNumber);
+    }
+  }
+
+  thumbnailsStyleChange(currentPage: number) {
+    this.previousItem.className = '';
+    this.previousItem = document.getElementById(currentPage - 1 + '')!;
+    this.previousItem.scrollIntoView({ behavior: 'auto', block: 'center' });
+    this.previousItem.className = 'selected';
+  }
+
+  openSignpad(modal: any) {
+    const modalRef = this.modalService.open(modal, { size: 'xl' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { FormsModule } from '@angular/forms';
+import { SignaturePadComponent } from './signature-pad/signature-pad.component';
+import { CommonModule } from '@angular/common';
+import { CheckBoxModule, ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { ColorPickerModule, UploaderModule, SignatureModule, TextBoxModule, NumericTextBoxModule } from '@syncfusion/ej2-angular-inputs';
+import { TabModule, ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignaturePadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxExtendedPdfViewerModule,
+    FormsModule,
+    CommonModule,
+    ColorPickerModule,
+    CheckBoxModule,
+    DropDownListModule,
+    TabModule,
+    ToolbarModule,
+    UploaderModule,
+    SignatureModule,
+    TextBoxModule,
+    DialogModule,
+    ButtonModule,
+    NumericTextBoxModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
