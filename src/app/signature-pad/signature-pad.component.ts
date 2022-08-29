cc9fa457ffe9df7926b8172d1c9c8ebe5e81fa5b
unknown <nguyentienhaininh@gmail.com>
2022-08-29 22:07:20 +0700

    update

--- src/app/signature-pad/signature-pad.component.ts
+++ src/app/signature-pad/signature-pad.component.ts
@@ -1,21 +1,9 @@
 import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
-import { addClass, createElement, getComponent } from '@syncfusion/ej2-base';
+import { getComponent } from '@syncfusion/ej2-base';
 import { SignatureComponent } from '@syncfusion/ej2-angular-inputs';
-import {
-  SplitButton,
-  ItemModel,
-  MenuEventArgs,
-} from '@syncfusion/ej2-splitbuttons';
 import { ClickEventArgs } from '@syncfusion/ej2-navigations';
-import { Button, ChangeEventArgs, CheckBox } from '@syncfusion/ej2-buttons';
-import {
-  ColorPicker,
-  ColorPickerEventArgs,
-  PaletteTileEventArgs,
-  Signature,
-  SignatureFileType,
-} from '@syncfusion/ej2-inputs';
-import { DropDownList } from '@syncfusion/ej2-angular-dropdowns';
+import { Button } from '@syncfusion/ej2-buttons';
+import { Signature } from '@syncfusion/ej2-inputs';
 
 @Component({
   selector: 'app-signature-pad',
@@ -25,176 +13,20 @@ import { DropDownList } from '@syncfusion/ej2-angular-dropdowns';
 })
 export class SignaturePadComponent {
   @ViewChild('signature') signature: SignatureComponent | any;
-  public strokeWidth: number = 2;
-  public items: ItemModel[] = [
-    {
-      text: 'Png',
-    },
-    {
-      text: 'Jpeg',
-    },
-    {
-      text: 'Svg',
-    },
-  ];
-
-  public templateCheckbox1: any = new CheckBox({
-    label: 'Disabled',
-    checked: false,
-    change: (args: ChangeEventArgs) => {
-      this.signature.disabled = args.checked;
-    },
-  });
-
   public thumbnails: string = '';
-
-  public colorPicker = ['red', 'green', 'blue'];
+  public colorPicker = ['#000000', 'red', 'blue'];
+  public widthSelector = [1, 2, 3];
 
   public change(): void {
     let signature: Signature = getComponent(
       document.getElementById('signature')!,
       'signature'
     );
-
-    let saveBtn: SplitButton = getComponent(
-      document.getElementById('save-option')!,
-      'split-btn'
-    );
-    if (!this.signature.isEmpty()) {
-      this.clearButton();
-      saveBtn.disabled = false;
-    }
     this.updateUndoRedo();
-
     this.thumbnails = this.signature.getSignature();
   }
   public onCreate(e: any) {
     this.thumbnails = this.signature.getSignature();
-    this.templateCheckbox1.appendTo('#chkelement1');
-    let ddl: DropDownList = new DropDownList({
-      dataSource: [1, 2, 3, 4, 5],
-      width: '60',
-      value: 2,
-      change: function (args) {
-        let signature: Signature = getComponent(
-          document.getElementById('signature')!,
-          'signature'
-        );
-        signature.maxStrokeWidth = args.value;
-      },
-    });
-    ddl.appendTo('#stroke-width');
-    new SplitButton(
-      {
-        iconCss: 'e-sign-icons e-save',
-        items: this.items,
-        content: 'Lưu',
-        select: (args: MenuEventArgs) => {
-          this.signature.save(args.item.text as SignatureFileType, 'Signature');
-        },
-        disabled: true,
-      },
-      '#save-option'
-    );
-    let strokeColor: ColorPicker = new ColorPicker({
-      modeSwitcher: false,
-      columns: 4,
-      presetColors: {
-        custom: [
-          '#000000',
-          '#e91e63',
-          '#9c27b0',
-          '#673ab7',
-          '#2196f3',
-          '#03a9f4',
-          '#00bcd4',
-          '#009688',
-          '#8bc34a',
-          '#cddc39',
-          '#ffeb3b',
-          '#ffc107',
-        ],
-      },
-      beforeTileRender: (args: PaletteTileEventArgs) => {
-        args.element.classList.add('e-circle-palette');
-        args.element.appendChild(
-          createElement('span', { className: 'e-circle-selection' })
-        );
-      },
-      showButtons: false,
-      mode: 'Palette',
-      cssClass: 'e-stroke-color',
-      change: (args: ColorPickerEventArgs) => {
-        if (this.signature.disabled) {
-          return;
-        }
-        let selElem: HTMLElement =
-          strokeColor.element.nextElementSibling?.querySelector(
-            '.e-selected-color'
-          ) as HTMLElement;
-        selElem.style.borderBottomColor = args.currentValue.rgba;
-        this.signature.strokeColor = args.currentValue.rgba;
-      },
-    });
-    strokeColor.appendTo('#stroke-color');
-    // let bgColor: ColorPicker = new ColorPicker({
-    //   modeSwitcher: false,
-    //   columns: 4,
-    //   noColor: true,
-    //   presetColors: {
-    //     custom: [
-    //       '#ffffff',
-    //       '#f44336',
-    //       '#e91e63',
-    //       '#9c27b0',
-    //       '#673ab7',
-    //       '#2196f3',
-    //       '#03a9f4',
-    //       '#00bcd4',
-    //       '#009688',
-    //       '#8bc34a',
-    //       '#cddc39',
-    //       '#ffeb3b',
-    //     ],
-    //   },
-    //   beforeTileRender: (args: PaletteTileEventArgs) => {
-    //     args.element.classList.add('e-circle-palette');
-    //     args.element.appendChild(
-    //       createElement('span', { className: 'e-circle-selection' })
-    //     );
-    //   },
-    //   showButtons: false,
-    //   mode: 'Palette',
-    //   cssClass: 'e-bg-color',
-    //   change: (args: ColorPickerEventArgs) => {
-    //     if (this.signature.disabled) {
-    //       return;
-    //     }
-    //     let selElem: HTMLElement =
-    //       bgColor.element.nextElementSibling?.querySelector(
-    //         '.e-selected-color'
-    //       ) as HTMLElement;
-    //     this.signature.backgroundColor = args.currentValue.rgba;
-    //     selElem.style.borderBottomColor = args.currentValue.rgba;
-    //   },
-    // });
-    // bgColor.appendTo('#bg-color');
-
-    addClass(
-      [
-        strokeColor.element.nextElementSibling?.querySelector(
-          '.e-selected-color'
-        )!,
-      ],
-      'e-sign-icons'
-    );
-    // addClass(
-    //   [bgColor.element.nextElementSibling?.querySelector('.e-selected-color')!],
-    //   'e-sign-icons'
-    // );
-    document
-      .getElementById('save-option')
-      ?.addEventListener('click', this.saveBtnClick);
     this.clearButton();
     let toolbarlItems: NodeListOf<Element> = document.querySelectorAll(
       '.e-toolbar .e-toolbar-items .e-toolbar-item .e-tbar-btn.e-tbtn-txt'
@@ -217,11 +49,7 @@ export class SignaturePadComponent {
     }
   }
 
-  public clicked(args: ClickEventArgs): void {
-    let saveBtn: SplitButton = getComponent(
-      document.getElementById('save-option')!,
-      'split-btn'
-    );
+  clicked(args: ClickEventArgs): void {
     if (this.signature.disabled && args.item.tooltipText != 'Disabled') {
       return;
     }
@@ -230,35 +58,24 @@ export class SignaturePadComponent {
         if (this.signature.canUndo()) {
           this.signature.undo();
           this.updateUndoRedo();
-          this.updateSaveBtn();
         }
         break;
       case 'Làm lại (Ctrl + Y)':
         if (this.signature.canRedo()) {
           this.signature.redo();
           this.updateUndoRedo();
-          this.updateSaveBtn();
         }
         break;
       case 'Xóa':
         this.signature.clear();
         if (this.signature.isEmpty) {
           this.clearButton();
-          saveBtn.disabled = true;
         }
         break;
     }
   }
 
-  public saveBtnClick(): void {
-    let signature: Signature = getComponent(
-      document.getElementById('signature')!,
-      'signature'
-    );
-    signature.save();
-  }
-
-  public clearButton() {
+  clearButton() {
     let tlItems: NodeListOf<Element> = document.querySelectorAll(
       '.e-toolbar .e-toolbar-items .e-toolbar-item .e-tbar-btn.e-tbtn-txt'
     );
@@ -274,17 +91,7 @@ export class SignaturePadComponent {
     }
   }
 
-  public updateSaveBtn() {
-    let saveBtn: SplitButton = getComponent(
-      document.getElementById('save-option')!,
-      'split-btn'
-    );
-    if (this.signature.isEmpty()) {
-      saveBtn.disabled = true;
-    }
-  }
-
-  public updateUndoRedo() {
+  updateUndoRedo() {
     let undoButton!: Button;
     let redoButton!: Button;
     let tlItems: NodeListOf<Element> = document.querySelectorAll(
@@ -312,5 +119,9 @@ export class SignaturePadComponent {
 
   chooseColor(color: string) {
     this.signature.strokeColor = color;
+  }
+
+  chooseWidth(width: number) {
+    this.signature.maxStrokeWidth = width;
   }
 }
